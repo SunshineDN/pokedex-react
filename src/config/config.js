5953b83213a6b21d5a26fa7860ae0e31a26e5ff2
SunshineDN <sunshinedn2003@gmail.com>
2023-05-11 21:25:29 -0300

    removido arquivos do docker

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  'pokedexdb',
-  'pokedex-react',
-  'poke321',
-  {
-    host: 'db',
-    dialect: 'postgres',
-  }
-);
-// const sequelize = new Sequelize(process.env.DATABASE_URL);
+// const sequelize = new Sequelize(
+//   'pokedexdb',
+//   'pokedex-react',
+//   'poke321',
+//   {
+//     host: 'db',
+//     dialect: 'postgres',
+//   }
+// );
+const sequelize = new Sequelize("postgres://cprxustl:vZfjowvWOhEd4ceLnKElh_C40QoFGxSp@silly.db.elephantsql.com/cprxustl");
 
 module.exports = sequelize;
