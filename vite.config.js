0462179eafa452fe01af177a5c9de9ee030ab283
Douglas Augusto <sunshinedn2003@gmail.com>
2024-08-26 20:25:08 -0300

    base removed from netlify

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pokedex-react/',
+  // base: '/pokedex-react/',
   plugins: [svgr(), react()],
 })
