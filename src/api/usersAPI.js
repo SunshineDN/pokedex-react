946542584b341e130a820efa49bf81dc8bf418d9
Douglas Augusto <sunshinedn2003@gmail.com>
2024-08-26 18:42:39 -0300

    http

--- src/api/usersAPI.js
+++ src/api/usersAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const usersAPI = axios.create({
-  baseURL: "//aiatende.com:8001/users/",
+  baseURL: "http://aiatende.com:8001/users/",
 });
 
 export default usersAPI;
