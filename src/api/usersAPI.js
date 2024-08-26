a26e7599f86a6b954903d183dc7817edc00738cd
Douglas Augusto <sunshinedn2003@gmail.com>
2024-08-26 18:32:16 -0300

    unauth remove

--- src/api/usersAPI.js
+++ src/api/usersAPI.js
@@ -1,11 +1,7 @@
 import axios from "axios";
-import https from "https";
 
 const usersAPI = axios.create({
   baseURL: "http://aiatende.com:8001/users/",
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false,
-  }),
 });
 
 export default usersAPI;
