626de6bca79806d0490a8ad674acf06062aa00a6
Douglas Augusto <sunshinedn2003@gmail.com>
2024-08-26 16:54:17 -0300

    rejectUnauthorized

--- src/api/usersAPI.js
+++ src/api/usersAPI.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import https from "https";
 
 const usersAPI = axios.create({
+  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
   baseURL: "http://aiatende.com:8001/users/",
 });
 
