8aedbdc1441e96322f1a48e582f925329431760c
Douglas Augusto <sunshinedn2003@gmail.com>
2024-08-26 16:33:20 -0300

    aiatende

--- src/api/usersAPI.js
+++ src/api/usersAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const usersAPI = axios.create({
-  baseURL: "https://pokedex-react-thl7.onrender.com/users/",
+  baseURL: "http://aiatende.com:8001/users/",
 });
 
 export default usersAPI;
