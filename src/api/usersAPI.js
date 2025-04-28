b04cff845f35fba7b7bd1d253a999d6f7a6b0e2c
Douglas Augusto <sunshinedn2003@gmail.com>
2025-04-28 13:07:52 -0300

    localhost altered

--- src/api/usersAPI.js
+++ src/api/usersAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const usersAPI = axios.create({
-  baseURL: "https://pokedex-react-thl7.onrender.com/users/"
+  baseURL: "http://localhost:8000/users/"
 });
 
 export default usersAPI;
